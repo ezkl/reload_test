911e35bfdb4562db872899b2641a2365b8ba837a
Ezekiel Templin <ezkl@me.com>
2011-07-13 19:34:28 -0400

    Initial commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,5 @@
+class PagesController < ApplicationController
+  def index
+    @variable = "Variable's Value"
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ReloadTest::Application.config.secret_token = '7f175c7d8259ff90f2a1af00ce2ab1bdbcdc6a8ee51a993f441e8847186940a9769a0e3dd31413c0c9094761de43afbd12577ee4ba6173c59e907e511c27b9f4'
